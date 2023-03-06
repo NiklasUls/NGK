4a54a2299e8211a474c2e0ffeaf17e1436c9a795
Niklas Ulsø <Niklasuls@live.dk>
2023-03-06 13:16:04 +0100

    Clientdel af øvelse 7

--- øvelse7/client/client.c
+++ øvelse7/client/client.c
@@ -13,7 +13,7 @@ Modified: Michael Alrøe
 #include <netdb.h> 
 #include "iknlib.h"
 
-#define BUFSIZE 1000
+#define BUFSIZE 256
 
 void error(const char *msg)
 {
@@ -23,70 +23,32 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-	printf("Starting client...\n");
 
 	int sockfd, portno, n;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
-	uint8_t buffer[BUFSIZE];
-	FILE * fp;
-	ssize_t numBytes;
+	unsigned int length;
+	struct sockaddr_in server, from;
+	struct hostent *hp;
 	char bufferr[BUFSIZE];
-	int bytes_written = 0;
     
-	if (argc < 3)
-	    error( "ERROR usage: ""hostname"",  ""port""");
+	if(argc != 4)error("Usage: server port\n");
 
-	portno = atoi(argv[2]);
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) 
-	    error("ERROR opening socket");
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd < 0) error("socket");
 
-	server = gethostbyname(argv[1]);
-	if (server == NULL) 
-	    error("ERROR no such host");
+	server.sin_family = AF_INET;
+	hp = gethostbyname(argv[1]);
+	if(hp == 0) error("Unknown host\n");
 
-	printf("Server at: %s, port: %s\n",argv[1], argv[2]);
-
-	printf("Connect...\n");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-	serv_addr.sin_port = htons(portno);
-	if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-	    error("ERROR connecting");
-
-	printf("Please enter the message: ");
-	char input[256];
-	fgets(input, sizeof(input),stdin);
-	input[strcspn(input, "\n")]=0;
-	n = write(sockfd, input, sizeof(input));
-	if(n == 0) error("Failed to send filename to server\n");
-	
-	const char* fileName = extractFileName(input);	//Extract filename from user-input.
-	long fileSize = readFileSizeTCP(sockfd);	//Read filesize wich is send from server
-	if (fileSize == -1) 
-	    error("File doesnt exist");
-	printf("\nFilename: %s, Filesize: %ld bytes\n", fileName, fileSize);
+	bcopy((char*)hp->h_addr, (char*)&server.sin_addr, hp->h_length);
+	server.sin_port = htons(atoi(argv[2]));
+	length = sizeof(struct sockaddr_in);
+	n=sendto(sockfd, argv[3], strlen( argv[3]), 0, (const struct sockaddr *)&server, length);
+	if(n<0) error("Sendto\n");
+	n=recvfrom(sockfd, bufferr, BUFSIZE, 0, (struct sockaddr *)&from, &length);
+	if(n<0) error("Recvfrom\n");
+	printf(bufferr);
 
 
-	
-	fp = fopen(fileName, "wb");
-	int bytes_to_recieve=0;
-	int i=1;
-	while (1)
-	{
-		bytes_to_recieve = ((BUFSIZE*i++>fileSize)? (fileSize%BUFSIZE) : BUFSIZE);
-		numBytes = read(sockfd, bufferr, bytes_to_recieve);
-		printf("Bytes to recieve: %i. numBytes: %zu\n", bytes_to_recieve, numBytes);
-		fwrite(bufferr, 1, numBytes, fp);
-		bzero(bufferr, BUFSIZE);
-		if(bytes_to_recieve != BUFSIZE) break;
-	}
-	
-	fclose(fp);
-
-    printf("Closing client...\n\n");
 	close(sockfd);
 	return 0;
 }
